da1b94ace6593f06c1a537951bb4f97819ace161
lfenzo <lfenzo@orotonmail.com>
2022-12-03 12:18:45 -0300

    feat: advances in the model baseline implementation

--- src/model_baselines.jl
+++ src/model_baselines.jl
@@ -1,15 +1,16 @@
 using CSV
+using Flux
 using Dates
 using FileIO
 using DataFrames
 using Statistics
 using ShiftedArrays
+using BenchmarkTools
 
 
 const ARTIFACT_DIR = joinpath(pwd(), "..", "artifacts")
 
 
-# abstract baseline
 abstract type Baseline end
 
 
@@ -23,7 +24,7 @@ mutable struct HourlyMean <: Baseline
     lookup_table::Union{AbstractDataFrame, Nothing}
     site_specific::Bool
 end
-HourlyMean(; site_specific) = HourlyMean(nothing, site_specific)
+HourlyMean(; site_specific = false) = HourlyMean(nothing, site_specific)
 
 function fit!(baseline::HourlyMean, X::DataFrame) :: Nothing
     agg_cols = baseline.site_specific ? [:id, :hour] : [:hour]
@@ -47,7 +48,7 @@ mutable struct DailyMean <: Baseline
     lookup_table::Union{AbstractDataFrame, Nothing}
     site_specific::Bool
 end
-DailyMean(; site_specific) = DailyMean(nothing, site_specific)
+DailyMean(; site_specific = false) = DailyMean(nothing, site_specific)
 
 function fit!(baseline::DailyMean, X::DataFrame) :: Nothing
     agg_cols = baseline.site_specific ? [:id, :doy] : [:doy]
@@ -71,7 +72,7 @@ mutable struct DateTimeMean <: Baseline
     lookup_table::Union{AbstractDataFrame, Nothing}
     site_specific::Bool
 end
-DateTimeMean(; site_specific) = DateTimeMean(nothing, site_specific)
+DateTimeMean(; site_specific = false) = DateTimeMean(nothing, site_specific)
 
 function fit!(baseline::DateTimeMean, X::DataFrame) :: Nothing
     agg_cols = baseline.site_specific ? [:id, :doy, :hour] : [:doy, :hour]
@@ -89,7 +90,7 @@ end
     PreviousDay()
 
 The value for the target in t+1 will be the same value for the target in the same hour
-of the previous day.
+of the previous day. This baseline is inherently site-specific.
 """
 mutable struct PreviousDay <: Baseline
     lookup_table::Union{AbstractDataFrame, Nothing}
@@ -101,34 +102,32 @@ function fit!(baseline::PreviousDay, X::DataFrame) :: Nothing
     # similar dataframe, but copying no rows from it
     baseline.lookup_table = similar(X, 0)
 
-    # reconstructing the missing 
+    # base dataframe for the 24h window alignment
+    start_date = DateTime(minimum(X.year)) + Day(minimum(X.doy) - 1)
+    end_date = DateTime(maximum(X.year)) + Day(maximum(X.doy) - 1)
+    datearray = start_date:Hour(1):end_date
+
+    fulltime_df = DataFrame(
+        hour = hour.(datearray),
+        doy = dayofyear.(datearray),
+        year = year.(datearray),
+    )
+
     for site in groupby(X, :id)
-        start_date = DateTime(minimum(site.year)) + Day(minimum(site.doy) - 1)
-        end_date = DateTime(maximum(site.year)) + Day(maximum(site.doy) - 1)
 
-        # construct a date array that will be used to fill the gaps in each of the sites
-        datearray = start_date:Hour(1):end_date
-
-        cols = [:id, :hour, :doy, :year]
-        local_df = similar(select(baseline.lookup_table, cols), 0)
-
-        local_df = DataFrame(
-            id = [site[1, :id] for _ in 1:length(datearray)],
-            hour = hour.(datearray),
-            doy = dayofyear.(datearray),
-            year = year.(datearray),
-        )
-
-        filled_df = leftjoin(local_df, X, on = cols)
+        filled_df = leftjoin(fulltime_df, site, on = [:hour, :doy, :year])
 
         # this sort operation is here to make sure that the correct chronological order
         # is kept after the join. The fact that the rows in the resulting dataframe get
         # somehow reordered makes absolutely no sense to me
-        sort!(filled_df, [:id, :year, :doy, :hour])
+        sort!(filled_df, [:year, :doy, :hour])
 
-        # now that every day worth of observations have the exact same number of hours
+        # now that every day worth of observations has the exact same number of hours
         # we can safely shift the values for the radiation
         filled_df[!, :baseline] = ShiftedArrays.lag(filled_df.next_radiation, 24)
+
+        # inserting back the :id column not present in `fulldate_df`
+        filled_df[!, :id] .= first(site.id)
 
         append!(baseline.lookup_table, filled_df, cols = :union)
     end
@@ -143,13 +142,47 @@ function predict(baseline::PreviousDay, X::DataFrame)
 end
 
 
-function main()
-    xtrain, ytrain = FileIO.load(joinpath(ARTIFACT_DIR, "artifacts.jld2"), "xtrain", "ytrain")
-
-    baseline = PreviousDay()
-    fit!(baseline, ytrain)  # yes, we are using ytrain, not xtrain...
-
-    return predict(baseline, ytrain)
+function evaluate_on_general_metrics(data::DataFrame) :: Dict
+    df = dropmissing(data)
+    return Dict(
+        "mae" => Flux.Losses.mae(df.next_radiation, df.baseline),
+        "rmse" => sqrt(Flux.Losses.mse(df.next_radiation, df.baseline)),
+    )
 end
 
-main()
+
+function main()
+    ytrain, ytest = FileIO.load(joinpath(ARTIFACT_DIR, "artifacts.jld2"), "ytrain", "ytest")
+
+    baseline_results = DataFrame(
+        :baseline => [],
+        :mae => [],
+        :rmse => [],
+        :site_specific => [],
+    )
+
+    for baseline in [PreviousDay, HourlyMean, DailyMean, DateTimeMean]
+        baseline_models = [baseline()]
+
+        if hasproperty(baseline_models[1], :site_specific)
+            push!(baseline_models, baseline(site_specific = true))
+        end
+
+        for model in baseline_models
+            # ytrain here is just a name convention, the columns that we need to fit the baseline
+            # models match the ones in ytrain.
+            fit!(model, ytrain)
+            baseline_predictions = predict(model, ytest)
+
+            metrics = evaluate_on_general_metrics(baseline_predictions)
+
+            site_specific = hasproperty(model, :site_specific) ? model.site_specific : nothing 
+            push!(baseline_results, (typeof(model), metrics["mae"], metrics["rmse"], site_specific))
+        end
+    end
+
+    sort!(baseline_results, :rmse)
+    @info baseline_results
+end
+
+@time main()
